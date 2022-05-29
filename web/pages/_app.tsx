5f6e9b43b0754fc10ba947775cb31da6c6898db5
Shubham Mehra <54092258+shubhammehra4@users.noreply.github.com>
2022-05-29 22:39:54 +0530

    Changes

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "regenerator-runtime/runtime";
 import CheckoutContextWrapper from "../context/checkout";
@@ -12,24 +13,47 @@ function MyApp({ Component, pageProps }: AppProps) {
     <QueryClientProvider client={queryClient}>
       <ChakraProvider>
         <CheckoutContextWrapper>
-          <meta name="application-name" content="Shop Mart" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta
-            name="apple-mobile-web-app-status-bar-style"
-            content="default"
-          />
-          <meta name="apple-mobile-web-app-title" content="Shop Mart" />
-          <meta
-            name="description"
-            content="One stop solution for all shopping needs"
-          />
-          <meta name="format-detection" content="telephone=no" />
-          <meta name="mobile-web-app-capable" content="yes" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/vercel.svg" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/vercel.svg" />
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="mask-icon" href="/vercel.svg" color="#5bbad5" />
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <Head>
+            <meta name="application-name" content="Shop Mart" />
+            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta
+              name="apple-mobile-web-app-status-bar-style"
+              content="default"
+            />
+            <meta name="apple-mobile-web-app-title" content="Shop Mart" />
+            <meta
+              name="description"
+              content="One stop solution for all shopping needs"
+            />
+            <meta name="format-detection" content="telephone=no" />
+            <meta name="mobile-web-app-capable" content="yes" />
+            <meta name="theme-color" content="#000000" />
+
+            <link rel="apple-touch-icon" href="/shopping-cart-48.png" />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/shopping-cart-48.png"
+            />
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/shopping-cart-48.png"
+            />
+            <link rel="manifest" href="/manifest.json" />
+            <link
+              rel="mask-icon"
+              href="/shopping-cart-48.png"
+              color="#5bbad5"
+            />
+            <link rel="shortcut icon" href="/shopping-cart-48.png" />
+            <meta
+              name="viewport"
+              content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+            />
+          </Head>
           <Component {...pageProps} />;
         </CheckoutContextWrapper>
       </ChakraProvider>
